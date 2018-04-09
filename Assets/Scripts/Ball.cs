82427355a629f8320f43d0f08bbf80826d527b4d
ChewyMicha <34125133+Chewy420@users.noreply.github.com>
2018-04-09 15:10:36 +0200

    ball trys

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
+	Rigidbody2D rb;
+	public float speed;
 	// Use this for initialization
+
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
 	void Start () {
 		// yo
+		rb.velocity = rb.velocity + Vector2.up;
 		
 	}
 	
